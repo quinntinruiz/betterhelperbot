75ff78ba416b54c85abe3b4c89b3ba006263749e
Quinntin Ruiz <[quinntin.ruiz@gmail.com]>
2017-07-14 16:32:56 -0700

    bug fixes

--- backend/models.js
+++ backend/models.js
@@ -1,38 +1,43 @@
-import mongoose from 'mongoose';
-var connect = process.env.MONGODB_URI;
-var Schema = mongoose.Schema;
-mongoose.connect(connect);
-mongoose.connection.on('connected',function(){
-  console.log("connnected to mongodb")
-})
-
-var groupSchema = new Schema({
-  members: {
-    type: mongoose.Schema.ObjectId,
-    ref:"Student"
-  },
-  assignment: String,
-  part: String,
-  problem: String,
-  location: String
-})
-
-var studentSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  group: {
-    type: mongoose.Schema.ObjectId,
-    ref:"Group"
-  }
-})
-
-
-
-
-var models = {
-  Group: mongoose.model("Group",groupSchema),
-  Student: mongoose.model("Student",studentSchema)
-}
-
-export default models
+// // import mongoose from 'mongoose';
+// var mongoose = require('mongoose');
+// var connect = process.env.MONGODB_URI;
+// var Schema = mongoose.Schema;
+//
+//
+// mongoose.connect(connect);
+// mongoose.connection.on('connected',function(){
+//   console.log("connnected to mongodb")
+// })
+//
+// var groupSchema = new Schema({
+//   members: {
+//     type: mongoose.Schema.ObjectId,
+//     ref:"Student"
+//   },
+//   assignment: String,
+//   part: String,
+//   problem: String,
+//   location: String
+// })
+//
+// var studentSchema = new Schema({
+//   name: String,
+//   email: String,
+//   password: String,
+//   group: {
+//     type: mongoose.Schema.ObjectId,
+//     ref:"Group"
+//   }
+// })
+//
+// var models = {
+//   Group: mongoose.model("Group",groupSchema),
+//   Student: mongoose.model("Student",studentSchema)
+// }
+//
+//
+// module.exports = models
+//
+//
+//
+// // export default models
--- backend/routes.js
+++ backend/routes.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const router = express.Router();
-var mongoose = require('mongoose');
-var Student = mongoose.model('Student', studentSchema)
-var Group = mongoose.model('Group', groupSchema)
-
-// YOUR API ROUTES HERE
-
-
-
-router.post('/localhost:3030/addGroup', (req, res)=>{
-    var newGroup= new Group({
-        assignment: req.body.assignment,
-        part: req.body.part,
-        problem: req.body.problem,
-        location: req.body.location
-    })
-    newGroup.save(function(err, result){
-        if(err){
-          callback(err);
-        } else{
-            callback(null, result);
-        }
-})
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// var mongoose = require('mongoose');
+// // import {Group, Student} from './models'
+// // import models from './models'
+// // var Student = models.Student;
+// // var Group = models.Group;
+// // YOUR API ROUTES HERE
+//
+//
+// var models = require('./models');
+// var Student = models.Student;
+// var Group = models.Group;
+//
+//
+//
+//
+//
+// router.post('/localhost:3000/addGroup', (req, res)=>{
+//     var newGroup= new Group({
+//         assignment: req.body.assignment,
+//         part: req.body.part,
+//         problem: req.body.problem,
+//         location: req.body.location
+//     })
+//     newGroup.save(function(err, result){
+//         if(err){
+//           callback(err);
+//         } else{
+//             callback(null, result);
+//         }
+//       })
+//     })
+// module.exports = router;
