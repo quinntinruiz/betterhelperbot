3cd0b7353e5b58f15774f850a839c26a449b1418
Xhonia Robinson <XDAWG@Xhonias-MacBook-Pro.local>
2017-07-14 13:56:49 -0700

    added models, and gitignore

---
+++ backend/models.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+var connect = process.env.MONGODB_URI;
+var Schema = mongoose.Schema;
+mongoose.connect(connect);
+mongoose.connection.on('connected',function(){
+  console.log("connnected to mongodb")
+})
+
+var groupSchema = new Schema({
+  membbers: {
+    type: mongoose.Schema.ObjectId,
+    ref:"Student"
+  } 
+  assignment: String,
+  purpose: String,
+  location: String
+})
+
+var studentSchema = new Schema({
+  name: String,
+  group: {
+    type: mongoose.Schema.ObjectId,
+    ref:"Group"
+  }
+})
+
+
+
+var models = {
+  Group: mongoose.model("Group",groupSchema),
+  Student: mongoose.model("Student",studentSchema)
+}
+
+export default models
